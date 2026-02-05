35fc3e7742b07db78c53ec57a138835e27b3afa5
declin93 <declin93@gmail.com>
2026-02-05 18:43:05 +0100

    Updated base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/habitus/',
+  base: command === 'serve' ? '/' : '/habitusApp/',
   plugins: [
     react(),
     VitePWA({
